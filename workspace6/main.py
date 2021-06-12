d07d512e3808cb80173cd25a92dfa6b1e0369f8e
negi <sakuryousuke1232@gmail.com>
2021-06-13 00:20:43 +0900

    １時間ごとに計算

--- workspace6/main.py
+++ workspace6/main.py
@@ -14,71 +14,52 @@ import ressave
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 n_epochs=1
-epsilon=0.1
+epsilon=0.9
 gamma=0.99
 learning_rate=0.5
 if __name__ == '__main__':
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
     G = ox.graph_from_bbox(  35.706, 35.6614, 140.0843, 139.9987,network_type='drive')
     nodedict,revnodedict=createdic.dic(G)
-    trajectories,max,min=createtrajectories.trajectories(nodedict)
-    
-    #print(trajectories)
-    
-    nS=len(list(G.nodes))
-    probs=createprobs.probs_dic(G,nodedict)
-    svf = StateVisitationFrequency(nS,nodedict,probs,max,min)#インスタンスを作成し、状態の数,アクションの数,タイル間で移動できるかの辞書を入力
-    #print(svf)
-    experts_feature = compute_experts_feature(nS, trajectories)
-    feature_matrix = np.eye(nS)
-    reward_function = Reward(nS)
-    value_iteration = ValueIteration(nS, 10, probs)
-    for i in  tqdm(range(n_epochs)):#20回ループする
-        pass
-        V, policy = value_iteration(gamma, epsilon, reward_function)
-        #重要訪れたことのない地点に報酬が発生した理由１ 訪れたことのない地点でのpolicyが発散したから
-        pass
-        # for i in V:
-        #     print(i)
-        # print("////////////////////////////")
-        P = svf(policy, trajectories)
-
-        pass
-        # for i in policy:
-        #     print(i)
-        grad = experts_feature - feature_matrix.T.dot(P)
-        #There is a problem in generating the action trajectory
+    rewards=list()
+    for hour in range(0,24):
+        trajectories,max,min=createtrajectories.trajectories(nodedict,hour)
         
-
-
-        # for i in range(len(P)):
-        #     #if grad[i] !=0:
-        #     print(i,P[i])
-        # print("////////////////////////////")
-
-        # for i in range(len(experts_feature)):
-        #     #if grad[i] !=0:
-        #     print(i,experts_feature[i])
-        # print("////////////////////////////")
-
-        # for i in range(len(feature_matrix.T.dot(P))):
-        #     #if grad[i] !=0:
-        #     print(i,feature_matrix.T.dot(P)[i])
-        # print("////////////////////////////")
-
-        # for i in range(len(grad)):
-        #     #if grad[i] !=0:
-        #     print(i,grad[i])
-        # print("////////////////////////////")
-
-
-        reward_function.update(learning_rate * grad)
+        #print(trajectories)
         
-        pass
-    
-    reward=reward_function(feature_matrix)
-    
-    ressave.save(reward,revnodedict)
+        nS=len(list(G.nodes))
+        probs=createprobs.probs_dic(G,nodedict)
+        svf = StateVisitationFrequency(nS,nodedict,probs,max,min)#インスタンスを作成し、状態の数,アクションの数,タイル間で移動できるかの辞書を入力
+        #print(svf)
+        experts_feature = compute_experts_feature(nS, trajectories)
+        feature_matrix = np.eye(nS)
+        reward_function = Reward(nS)
+        value_iteration = ValueIteration(nS, 10, probs)
+        
+        for i in  tqdm(range(n_epochs)):#20回ループする
+            pass
+            V, policy = value_iteration(gamma, epsilon, reward_function)
+            #重要訪れたことのない地点に報酬が発生した理由１ 訪れたことのない地点でのpolicyが発散したから
+            pass
+            # for i in V:
+            #     print(i)
+            # print("////////////////////////////")
+            P = svf(policy, trajectories)
+
+            pass
+            # for i in policy:
+            #     print(i)
+            grad = experts_feature - feature_matrix.T.dot(P)
+            #There is a problem in generating the action trajectory
+
+
+            reward_function.update(learning_rate * grad)
+            
+            pass
+        
+        reward=reward_function(feature_matrix)
+        rewards.append(reward)
+    ressave.save(rewards,revnodedict)
     # st=np.zeros(10000)
     # plt.cla()
     # aa=0
--- workspace6/ressave.py
+++ workspace6/ressave.py
@@ -11,14 +11,15 @@ def save(reward,revnodedict):#μ0(s0)を計算
     dbname = 'log.db'
     con = sqlite3.connect(dbname) #DBに接続
     cur = con.cursor()#カーソルを使いやすいように代入
-    create_sql='CREATE TABLE IF NOT EXISTS reward(seq INTEGER PRIMARY KEY AUTOINCREMENT,node INTEGER,reward node INTEGER)'
+    create_sql='CREATE TABLE IF NOT EXISTS reward(seq INTEGER PRIMARY KEY AUTOINCREMENT,node INTEGER,reward0 INTEGER,reward1 INTEGER,reward2 INTEGER,reward3 INTEGER,reward4 INTEGER,reward5 INTEGER,reward6 INTEGER,reward7 INTEGER,reward8 INTEGER,reward9 INTEGER,reward10 INTEGER,reward11 INTEGER,reward12 INTEGER,reward13 INTEGER,reward14 INTEGER,reward15 INTEGER,reward16 INTEGER,reward17 INTEGER,reward18 INTEGER,reward19 INTEGER,reward20 INTEGER,reward21 INTEGER,reward22 INTEGER,reward23 INTEGER)'
     cur.execute(create_sql)
-    insert_sql='INSERT INTO reward(node,reward) VALUES(?,?)'
+    insert_sql='INSERT INTO reward(node,reward0,reward1,reward2,reward3,reward4,reward5,reward6,reward7,reward8,reward9,reward10,reward11,reward12,reward13,reward14,reward15,reward16,reward17,reward18,reward19,reward20,reward21,reward22,reward23) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'
     insertlist=list()
-    for i in range(len(reward)):
+    #for i in range(len(reward[0])):
+    for i in range(8000):
         st=list()
-        print(i,revnodedict[i],reward[i])
-        insertlist.append([revnodedict[i],reward[i]])
+        #print(i,revnodedict[i],reward[i])
+        insertlist.append([revnodedict[i],reward[0][i],reward[1][i],reward[2][i],reward[3][i],reward[4][i],reward[5][i],reward[6][i],reward[7][i],reward[8][i],reward[9][i],reward[10][i],reward[11][i],reward[12][i],reward[13][i],reward[14][i],reward[15][i],reward[16][i],reward[17][i],reward[18][i],reward[19][i],reward[20][i],reward[21][i],reward[22][i],reward[23][i]])
         if (i + 1) % 1000 == 0:
                 #print(len(insertlist))
                 cur.executemany(insert_sql, insertlist)
